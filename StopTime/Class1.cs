3512f9dcd804616b2039c32cc7fb7621f8bf3a5a
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-07-25 23:46:16 +0100

    update

--- StopTime/Class1.cs
+++ StopTime/Class1.cs
@@ -10,49 +10,58 @@ namespace StopTime
 	{
 		private void Start()
 		{
-			base.ModHelper.Console.WriteLine("[In StopTime] :");
-			base.ModHelper.Events.Subscribe<Flashlight>(Events.AfterStart);
-			IModEvents events = base.ModHelper.Events;
+			ModHelper.Console.WriteLine("[In StopTime] :");
+
+			ModHelper.Events.Subscribe<Flashlight>(Events.AfterStart);
+			var events = ModHelper.Events;
 			events.OnEvent = (Action<MonoBehaviour, Events>)Delegate.Combine(events.OnEvent, new Action<MonoBehaviour, Events>(this.OnEvent));
 			GlobalMessenger.AddListener("LearnLaunchCodes", new Callback(this.SaveGame));
-			base.ModHelper.Console.WriteLine(": Disabling statue...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<MemoryUplinkTrigger>("OnTriggerEnter");
-			base.ModHelper.Console.WriteLine(": Disabling interloper destruction...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<TempCometCollisionFix>("Update");
-			base.ModHelper.Console.WriteLine(": Disabling starfield updates...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<StarfieldController>("Update");
-			base.ModHelper.Console.WriteLine(": Disabling sun expansion...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<SunController>("UpdateScale");
-			base.ModHelper.Console.WriteLine(": Disabling sun logic...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<SunController>("Update");
-			base.ModHelper.Console.WriteLine(": Disabling sun collapse SFX...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<SunController>("OnTriggerSupernova");
-			base.ModHelper.Console.WriteLine(": Disabling End Times music...");
-			base.ModHelper.HarmonyHelper.EmptyMethod<GlobalMusicController>("UpdateEndTimesMusic");
-			base.ModHelper.Console.WriteLine(": Patching GetSecondsElapsed...");
-			base.ModHelper.HarmonyHelper.AddPrefix<TimeLoop>("GetSecondsElapsed", typeof(Patches), "SandLevelPrefix");
+
+			ModHelper.Console.WriteLine(": Disabling statue...");
+			ModHelper.HarmonyHelper.EmptyMethod<MemoryUplinkTrigger>("OnTriggerEnter");
+
+			ModHelper.Console.WriteLine(": Disabling interloper destruction...");
+			ModHelper.HarmonyHelper.EmptyMethod<TempCometCollisionFix>("Update");
+
+			ModHelper.Console.WriteLine(": Disabling starfield updates...");
+			ModHelper.HarmonyHelper.EmptyMethod<StarfieldController>("Update");
+
+			ModHelper.Console.WriteLine(": Disabling sun expansion...");
+			ModHelper.HarmonyHelper.EmptyMethod<SunController>("UpdateScale");
+
+			ModHelper.Console.WriteLine(": Disabling sun logic...");
+			ModHelper.HarmonyHelper.EmptyMethod<SunController>("Update");
+
+			ModHelper.Console.WriteLine(": Disabling sun collapse SFX...");
+			ModHelper.HarmonyHelper.EmptyMethod<SunController>("OnTriggerSupernova");
+
+			ModHelper.Console.WriteLine(": Disabling End Times music...");
+			ModHelper.HarmonyHelper.EmptyMethod<GlobalMusicController>("UpdateEndTimesMusic");
+
+			ModHelper.Console.WriteLine(": Patching GetSecondsElapsed...");
+			ModHelper.HarmonyHelper.AddPrefix<TimeLoop>("GetSecondsElapsed", typeof(Patches), "SandLevelPrefix");
 		}
 
 		private void OnEvent(MonoBehaviour behaviour, Events ev)
 		{
-			bool flag = behaviour.GetType() == typeof(Flashlight) && ev == Events.AfterStart;
-			bool flag2 = flag;
+			var flag = behaviour.GetType() == typeof(Flashlight) && ev == Events.AfterStart;
+			var flag2 = flag;
 			if (flag2)
 			{
-				this.SaveGame();
-				base.ModHelper.Console.WriteLine(": Starting time loop...");
+				SaveGame();
+				ModHelper.Console.WriteLine(": Starting time loop...");
 				TimeLoop.SetTimeLoopEnabled(true);
-				base.ModHelper.Console.WriteLine(": Setting isTimeFlowing to false...");
-				typeof(TimeLoop).GetAnyField("_isTimeFlowing").SetValue(null, false);
-				base.ModHelper.Console.WriteLine(string.Format(": Sand-loop timescale set to {0}x", StopTime._debugTimeScale));
-				base.ModHelper.Console.WriteLine(string.Format(": Sand-loop length set to {0} minutes.", StopTime._LoopLength));
-				this._isStarted = true;
+				ModHelper.Console.WriteLine(": Setting isTimeFlowing to false...");
+				TimeLoop._isTimeFlowing = false;
+				ModHelper.Console.WriteLine(string.Format(": Sand-loop timescale set to {0}x", _debugTimeScale));
+				ModHelper.Console.WriteLine(string.Format(": Sand-loop length set to {0} minutes.", _LoopLength));
+				_isStarted = true;
 			}
 		}
 
 		private void SaveGame()
 		{
-			bool flag = PlayerData.KnowsLaunchCodes() && PlayerData.LoadLoopCount() == 1;
+			var flag = PlayerData.KnowsLaunchCodes() && PlayerData.LoadLoopCount() == 1;
 			if (flag)
 			{
 				PlayerData.SaveLoopCount(5);
@@ -62,7 +71,7 @@ namespace StopTime
 
 		public static float GetSecondsElapsed()
 		{
-			return StopTime._LoopLength * 60f - 2f * Math.Abs(Time.timeSinceLevelLoad * (StopTime._debugTimeScale - 0.5f) % (StopTime._LoopLength * 60f) - StopTime._LoopLength * 30f) + StopTime._debugTimeOffset;
+			return _LoopLength * 60f - 2f * Math.Abs(Time.timeSinceLevelLoad * (_debugTimeScale - 0.5f) % (_LoopLength * 60f) - _LoopLength * 30f) + _debugTimeOffset;
 		}
 
 		private static float _LoopLength = 22f;
--- StopTime/Patches.cs
+++ StopTime/Patches.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Harmony;
-
-namespace StopTime
+﻿namespace StopTime
 {
 	public static class Patches
 	{
